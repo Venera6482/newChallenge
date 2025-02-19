0451e40563ea44504cbfac3ae0feae9940b45033
Venera6482 <venerakockonova181@gmail.com>
2025-02-19 13:59:25 +0600

    maps

---
+++ maps/myMaps.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in golang")
+
+	languages := make(map[string]string)
+
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+
+	fmt.Println("List of all languages: ", languages)
+	fmt.Println("JS shorts for: ", languages["JS"])
+
+	delete(languages, "RB")
+}
